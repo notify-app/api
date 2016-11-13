2000a7cc91e0492887a604e2f7a50ecc221e3529
Luca Tabone <taboneluca3@gmail.com>
2016-11-13 14:24:54 +0100

    Catch errors from fortuneJS request listener

--- src/listeners/fortune.js
+++ src/listeners/fortune.js
@@ -1,15 +1,22 @@
 'use strict'
 
-const jsonAPISerializer = require('fortune-json-api')
 const createListener = require('fortune-http')
+const jsonAPISerializer = require('fortune-json-api')
 
 const notifyStore = require('../store')
 
-module.exports = createListener(notifyStore.store, {
+const listener = createListener(notifyStore.store, {
   serializers: [
-    [ jsonAPISerializer, {
-      inflectType: false,
-      inflectKeys: false
-    }]
+    [
+      jsonAPISerializer,
+      { inflectType: false, inflectKeys: false }
+    ]
   ]
 })
+
+module.exports = function (req, res) {
+  listener(req, res)
+    .catch(function (err) {
+      console.error(err)
+    })
+}
