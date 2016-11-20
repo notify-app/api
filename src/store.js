18e59a3b2c92fc87e9b88412a1b02e1953e3ee18
Luca Tabone <taboneluca3@gmail.com>
2016-11-20 18:18:01 +0100

    Include worker to notify store

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const {worker} = require('ipc-emitter')
 const store = require('notify-store')
 const config = require('../config')
 const auth = require('./auth')
@@ -7,6 +8,7 @@ const auth = require('./auth')
 const notifyStore = Object.create(store)
 
 notifyStore.init({
+  worker,
   url: config.db.url,
   authorize: auth
 })
