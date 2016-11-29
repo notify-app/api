e22bd21ceab8174ebd52ca4282864ff318e70e2b
Luca Tabone <taboneluca3@gmail.com>
2016-11-29 18:51:11 +0100

    Use shorthand JSON field

--- src/store.js
+++ src/store.js
@@ -3,14 +3,14 @@
 const {worker} = require('ipc-emitter')
 const store = require('notify-store')
 const config = require('../config')
-const auth = require('./auth')
+const authorize = require('./auth')
 
 const notifyStore = Object.create(store)
 
 notifyStore.init({
   worker,
-  url: config.db.url,
-  authorize: auth
+  authorize,
+  url: config.db.url
 })
 
 module.exports = notifyStore
