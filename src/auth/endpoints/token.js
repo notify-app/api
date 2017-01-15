9c0799a15bb043b04cbc597c172371ece1e17c4f
Luca Tabone <taboneluca3@gmail.com>
2017-01-15 23:12:03 +0100

    Restrict token creation

--- src/auth/endpoints/token.js
+++ src/auth/endpoints/token.js
@@ -1,11 +1,13 @@
 'use strict'
 
 const errors = require('../errors')
+const grants = require('../grants')
 
-module.exports = (requestOptions, user) => {
+
+module.exports = (requestOptions, user, notifyStore) => {
   switch (requestOptions.method) {
     // TOKENS can only be CREATED.
-    case 'create': return Promise.resolve()
+    case 'create': return authCreate(requestOptions, user, notifyStore)
 
     // TOKENS cannot be MODIFIED, DELETED and READ.
     default: return Promise.reject({
@@ -13,4 +15,21 @@ module.exports = (requestOptions, user) => {
       allowed: 'POST'
     })
   }
+}
+
+/**
+ * authCreate verifies that the consumer is allowed to create tokens.
+ * @param  {Object} requestOptions Info about HTTP Request.
+ * @param  {Object} user           Info about user.
+ * @param  {Object} notifyStore    Notify store instance.
+ * @return {Promise}               Resolved if the consumer is allowed to create
+ *                                 tokens. Rejected otherwise.
+ */
+function authCreate (requestOptions, user, notifyStore) {
+  return grants(notifyStore)
+    .then(grants => {
+      const canCreateToken = user.grants.indexOf(grants['CREATE_TOKEN']) !== -1
+      if (canCreateToken === true) return Promise.resolve()
+      return Promise.reject({ type: errors.UN_AUTHORIZED })
+    })
 }
