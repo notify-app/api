aeb50c5b9819df7cc2ecd63d63c022a69a7ecbcb
Luca Tabone <taboneluca3@gmail.com>
2017-01-15 10:42:24 +0100

    Allow tokens to be created via api
    Currently tokens were not allowed to be created using the HTTP
    Requests. However since it was decided that the auth should not be
    a core functionality of notify, this had to change.
    
    With this commit HTTP Consumers will be able to create tokens
    without however in the future this will be restricted by GRANTS,
    allowing only some users to do such requests.

--- src/auth/endpoints/token.js
+++ src/auth/endpoints/token.js
@@ -2,7 +2,15 @@
 
 const errors = require('../errors')
 
-module.exports = () => {
-  // TOKENS cannot be CRUD from API.
-  return Promise.reject({ type: errors.METHOD_NOT_ALLOWED })
+module.exports = (requestOptions, user) => {
+  switch (requestOptions.method) {
+    // TOKENS can only be CREATED.
+    case 'create': Promise.resolve()
+
+    // TOKENS cannot be MODIFIED, DELETED and READ.
+    default: return Promise.reject({
+      type: errors.METHOD_NOT_ALLOWED,
+      allowed: 'POST'
+    })
+  }
 }
