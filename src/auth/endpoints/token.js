a23f3548553fbccc64ff5454b2e6294ba8457f26
Luca Tabone <taboneluca3@gmail.com>
2017-02-10 13:06:03 +0100

    Enable Read CRUD operation for Tokens
    Currently the Tokens endpoint only allowed us to create tokens.
    This commit changes this to also enable us to read tokens. When
    reading tokens the server will only return tokens that belong to
    bots that are created by the consumer.

--- src/auth/endpoints/token.js
+++ src/auth/endpoints/token.js
@@ -6,12 +6,68 @@ const grants = require('../grants')
 
 module.exports = (requestOptions, user, notifyStore) => {
   switch (requestOptions.method) {
+    // Only tokens of Child Bots can be read.
+    case 'find': return authFind(requestOptions, user, notifyStore)
+
     // TOKENS can only be CREATED.
     case 'create': return authCreate(requestOptions, user, notifyStore)
 
     // TOKENS cannot be MODIFIED, DELETED or READ from JSONAPI endpoint.
     default: return authDefault()
   }
+}
+
+/**
+ * authFind verifies that the tokens returned belong to bots created by the
+ * consumer.
+ * @param  {Object} requestOptions Info about HTTP Request.
+ * @param  {Object} user           Info about user.
+ * @param  {Object} notifyStore    Notify store instance.
+ * @return {Promise}               Resolved if the consumer is allowed to create
+ *                                 tokens. Rejected otherwise.
+ */
+function authFind (requestOptions, user, notifyStore) {
+  // When the user doesn't request for specific tokens, it should only return
+  // tokens which belong to bots the user created himself.
+  let usersPromise = Promise.resolve(user.created)
+
+  // When theu user requests for specific tokens (using token ids) it should
+  // first retrieve the IDs of users who are the owners of the tokens requested
+  // and then filter these IDs to only have the IDs of users who have been
+  // created by the user himself.
+  if (requestOptions.ids !== null) {
+    // Retrieve tokens
+    usersPromise = notifyStore.store
+      .find(notifyStore.types.TOKENS, requestOptions.ids)
+      .then(({payload}) => {
+        const userIDs = []
+
+        // Store the IDs of users who were created by the user.
+        payload.records.forEach(token => {
+          if (user.created.indexOf(token.user) !== -1) userIDs.push(token.user)
+        })
+
+        return userIDs
+      })
+  }
+
+  // At this stage, we would have a list of user IDs which first of all are the
+  // owner of the tokens requested and secondly have been created by the
+  // consumer. Now all we have to do is to filter this list to only return the
+  // token IDs of bots.
+  return usersPromise
+    .then(userIDs => {
+      return notifyStore.store.find(notifyStore.types.USERS, userIDs)
+    })
+    .then(({payload}) => {
+      const tokenIDs = []
+
+      payload.records.forEach(childUser => {
+        if (childUser.bot === true) tokenIDs.push(childUser.token)
+      })
+
+      requestOptions.ids = tokenIDs
+    })
 }
 
 /**
