11ff7a01fc1e7d962fe1a8b27d51654e7d9ff7c4
Luca Tabone <taboneluca3@gmail.com>
2017-02-11 13:56:53 +0100

    Allow modifications of bot users
    Currently a user was restricted to update his own user. This commit
    changes this so that a user can also do modifications to bots that
    he has created.

--- src/auth/endpoints/user.js
+++ src/auth/endpoints/user.js
@@ -11,8 +11,8 @@ module.exports = (requestOptions, user, notifyStore) => {
     // USERS can be CREATED only if the creator is allowed to.
     case 'create': return authCreate(requestOptions, user, notifyStore)
 
-    // USERS can only be modified by the modifier.
-    case 'update': return authUpdate(requestOptions, user)
+    // USERS can only modify their own user or bots they have created.
+    case 'update': return authUpdate(requestOptions, user, notifyStore)
 
     // USERS cannot be deleted.
     case 'delete': return authDelete()
@@ -94,29 +94,79 @@ function authCreate (requestOptions, user, notifyStore) {
 }
 
 /**
- * authUpdate verifies that the consumer is updating his own info.
+ * authUpdate verifies that the consumer is either updating his own user or if
+ * he has the CREATE_BOT grant is updating a bot he has created.
  * @param  {Object} requestOptions Info about HTTP Request.
  * @param  {Object} user           Info about user.
  * @return {Promise}               Resolved when the consumer is updating his
  *                                 own details. Rejected otherwise.
  */
-function authUpdate (requestOptions, user) {
+function authUpdate (requestOptions, user, notifyStore) {
   // Remove modifications to restricted fields.
-  requestOptions.payload[0].replace.username
-  requestOptions.payload[0].replace.internalID
-  requestOptions.payload[0].replace.bot
-  requestOptions.payload[0].replace.grants
-  requestOptions.payload[0].replace.token
-  requestOptions.payload[0].replace.rooms
-  requestOptions.payload[0].replace.creator
-  requestOptions.payload[0].replace.created
-  requestOptions.payload[0].replace.messages
+  delete requestOptions.payload[0].replace.internalID
+  delete requestOptions.payload[0].replace.bot
+  delete requestOptions.payload[0].replace.grants
+  delete requestOptions.payload[0].replace.token
+  delete requestOptions.payload[0].replace.creator
+  delete requestOptions.payload[0].replace.created
+  delete requestOptions.payload[0].replace.messages
 
-  // Make sure that the user is updating his own info.
-  if (user.id === requestOptions.ids[0]) return Promise.resolve()
-  return Promise.reject({
-    type: errors.UN_AUTHORIZED,
-    message: 'You are only allowed to modify your own user'
+  /**
+   * isOwnUpdate indicates whether the user is updating his own user.
+   * @type {Boolean}
+   */
+  const isOwnUpdate = user.id === requestOptions.ids[0]
+
+  /**
+   * isChildUpdate indicates whether the user is updating a user he has created.
+   * @type {Boolean}
+   */
+  const isChildUpdate = user.created.indexOf(requestOptions.ids[0]) !== -1
+
+  // If the consumer is not updating his own user or a user he has created, stop
+  // the modify request.
+  if (!isOwnUpdate && !isChildUpdate) {
+    return Promise.reject({
+      type: errors.UN_AUTHORIZED,
+      message: 'You are allowed to modify your own user and your bots'
+    })
+  }
+  
+  if (isOwnUpdate) {
+    // Remove modifications to restricted fields.
+    delete requestOptions.payload[0].replace.rooms
+    return Promise.resolve()
+  }
+
+  // When the consumer is updating another user, we need to verify that:
+  //   * The user has CREATE_BOT grant
+  //   * The user is updating a bot.
+  return grants(notifyStore).then(grants => {
+    /**
+     * canCreateBot indicates whether the user has the CREATE_BOT grant.
+     * @type {Boolean}
+     */
+    const canCreateBot = (user.grants.indexOf(grants['CREATE_BOT']) !== -1)
+
+    // If the user cannot create bots, he cannot modify bots either and
+    // therefore we need to stop the modify request.
+    if (!canCreateBot) {
+      return Promise.reject({
+        type: errors.UN_AUTHORIZED,
+        message: 'You are only allowed to modify your own user'
+      })
+    }
+  }).then(() => {
+    const botID = requestOptions.ids[0]
+    return notifyStore.store.find(notifyStore.types.USERS, botID)
+  }).then(({payload}) => {
+    // If the user is not a bot, stop the modify request.
+    if (payload.records[0].bot === false) {
+      return Promise.reject({
+        type: errors.UN_AUTHORIZED,
+        message: 'You are allowed to modify your own user and your bots'
+      })
+    }
   })
 }
 
